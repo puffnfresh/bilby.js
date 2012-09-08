24c3d629ce1354c70c7686f7be26a083b2219be7
Anton Rissanen <hello@anton.fi>
2012-09-08 14:17:04 +0300

    Turn feature examples into functional tests

---
+++ test/features.js
@@ -0,0 +1,61 @@
+require('../bilby').bilbify(global);
+
+var add = curry(function(a, b) {
+    return a + b;
+});
+
+exports.testCurry = function(test) {
+    // Curried and uncurried application
+    test.equal( add(1)(2) , 3);
+    test.equal( add(1, 2) , 3);
+
+    test.done();
+};
+
+exports.testOption = function(test) {
+    test.equal( some(3).getOrElse(0) , 3);
+    test.equal( none.getOrElse(0) , 0);
+
+    test.done();
+};
+
+exports.testMonad = function(test) {
+    test.equal(
+        Do()(some(1) >= function(x) {
+            return x < 0 ? none : some(x + 2);
+        }).getOrElse(0),
+
+        3
+    );
+
+    test.done();
+};
+
+exports.kleisliTest = function(test) {
+    test.equal(
+        Do()(function(x) {
+            return x < 0 ? none : some(x + 1);
+        } >> function(x) {
+            return x % 2 !== 0 ? none : some(x + 1);
+        })(1).getOrElse(0),
+
+        3
+    );
+
+    test.done();
+};
+
+exports.functorTest = function(test) {
+    test.equal( Do()(some(1) > add(2)).getOrElse(0) , 3);
+    test.done();
+};
+
+exports.applicativeTest = function (test) {
+    test.equal( Do()(some(add) * some(1) * some(2)).getOrElse(0) , 3);
+    test.done();
+};
+
+exports.semigroupTest = function (test) {
+    test.equal( Do()(some(1) + some(2)).getOrElse(0) , 3);
+    test.done();
+};
